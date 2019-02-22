b8c05d46e1c9066cf8d36537897660f49acc610f
parkchanjong <7000cj@gmail.com>
2019-02-23 03:15:07 +0900

    funtion

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("일하는중!");
+const calculator = {
+  plus: function(a, b) {
+    return a + b;
+  },
+  minus: function(a, b) {
+    return a - b;
+  },
+  time: function(a, b) {
+    return a * b;
+  },
+  divided: function(a, b) {
+    return a / b;
+  },
+  power: function(a, b) {
+    return a ** b;
+  }
+};
+
+const plus = calculator.power(4, 2);
+console.log(plus);
