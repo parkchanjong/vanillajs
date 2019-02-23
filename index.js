fd6aeef525265468b037a0fae937a7b657896773
parkchanjong <7000cj@gmail.com>
2019-02-24 02:52:50 +0900

    classlist

--- index.js
+++ index.js
@@ -18,15 +18,70 @@ const calculator = {
 
 const plus = calculator.power(4, 2);
 console.log(plus);
-
-//const title = document.getElementById("title");
-const title = document.querySelector("#title");
-title.innerHTML = "hihihi";
-title.style.color = "black";
+//
+//
+//
+//
 document.title = "head";
+//const title = document.getElementById("title");
+//title.innerHTML = "hihihi";
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  title.style.color = "blue";
+  title.classList.toggle(CLICKED_CLASS);
+}
+/*  =toggle
+const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
+  } else {
+    title.classList.add(CLICKED_CLASS);
+  }
+  */
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+init();
+
+/*
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  console.log(currentColor);
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
 
-title.addEventListener("click", handleClick);
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init();
+*/
+
+//
+//
+//
+//
+/*
+const age = prompt("ask"); //bad
+
+if (age > 20 && age < 30) {
+  console.log("ok");
+} else if (age > 21) {
+  console.log("mm");
+} else {
+  console.log("no");
+}
+*/
